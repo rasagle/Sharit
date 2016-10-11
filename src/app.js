35935c6eaca677260ed0284a460e93b31185e083
hui huang <hui>
2016-10-11 11:50:03 -0400

    initial commit

---
+++ src/app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+//settting static files
+var staticPath = path.join(__dirname, 'public');
+app.use(express.static(staticPath));
+
+//setting view engine
+app.set('view engine', 'ejs');
+
+var port = process.env.PORT || 3000;
+
+//requiring routes
+var api = require('./routes.js');
+
+app.get('/', function(req, res){
+	res.render('index.ejs');
+});
+
+app.listen(port, function(){
+	console.log('Listening to port', port);
+});
