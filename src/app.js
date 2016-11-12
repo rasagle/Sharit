db393dd198a6d070008688a7165c1df23bd3be1d
Hui <hh1128@nyu.edu>
2016-11-12 17:02:44 -0500

    connected to postgres

--- src/app.js
+++ src/app.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var pg = require('pg');
 
 var app = express();
+var config = {
+	user: 'postgres',
+	database: 'Sharit',
+	password: 'root',
+	host: 'localhost',
+	port: 5432,
+	max: 100,
+	idleTimeoutMillis: 30000
+}
+var connect = "postgres://postgres:root@localhost/Sharit";
+
+var pool = new pg.Pool(config);
 
 //settting static files
 var staticPath = path.join(__dirname, 'public');
 app.use(express.static(staticPath));
+
+//use body-parser
+app.use(bodyParser.urlencoded({extended: false}));
 
 //setting view engine
 app.set('view engine', 'ejs');
@@ -17,7 +33,18 @@ var port = process.env.PORT || 3000;
 var api = require('./routes.js');
 
 app.get('/', function(req, res){
-	res.render('index.ejs');
+	pool.connect(function(err, client, done){
+		if(err)
+			return res.send('You goofed');
+		client.query('SELECT * FROM users.user', function(err, result){
+			if(err)
+				return res.send('query error');
+			done();
+			console.log(result);
+			res.render('index.ejs');
+		});
+	});
+
 });
 
 app.get('/*', function(req, res){
