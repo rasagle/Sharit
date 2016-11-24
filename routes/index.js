400a466b8eb3dfe9c715af9e63139ce63c172c55
Hui <hh1128@nyu.edu>
2016-11-24 18:39:11 -0500

    connected to db

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
+var pg = require('pg');
 var router = express.Router();
+
+var configDB = require('../config/dbconfig.js');
+var pool = new pg.Pool(configDB);
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	res.render('index');
+	
+	pool.connect(function(err, client, done) {
+		if (err) console.log('Failed to connect to database');
+		client.query('SELECT * FROM users.user', function(err, result) {
+			if (err) console.log('Query error');
+			done();
+			console.log(result);
+			res.render('index');
+	});
+});
 });
 
 module.exports = router;
