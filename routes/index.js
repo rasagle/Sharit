649cc93ef7009a5541094822490d688d1b885c47
Warlon Zeng <wz634@nyu.edu>
2016-12-02 22:43:21 -0500

    finally got db->res directly working

--- routes/index.js
+++ routes/index.js
@@ -243,16 +243,27 @@ router.get('/downloadFile', function(req, res){
 });
 
 
-// 1 user param: id (file id)
-// sends back binary data
 router.post('/downloadFile', function(req, res){
-	var downloadFile = 'SELECT data FROM posts.file WHERE id = $1';
 	console.log(req.body);
+	var getFilename = 'SELECT filename FROM posts.file WHERE thread_id = $1';
+
 	pool.connect(function(err, client, done){
-		client.query(downloadFile, [req.body.file_id], function(err, result){
-			done();
-    		fs.writeFile('/tmp/foo.pdf', result.rows[0].data);
-    		res.status(200).json( {success: true });
+		client.query(getFilename, [req.body.thread_id], function(err, result){
+			console.log(result.rows);
+			var filename = result.rows[0].filename;
+			var downloadFile = 'SELECT data FROM posts.file WHERE thread_id = $1';
+
+			client.query(downloadFile, [req.body.thread_id], function(err, result){
+				// console.log(result.rows); // massive wall of text
+				done();
+    			data = result.rows[0].data;
+      			res.writeHead(200, {
+        		'Content-Type': 'application/octet-stream',
+        		'Content-disposition': 'attachment;filename=' + filename,
+        		'Content-Length': data.length
+    			});
+    			res.end(new Buffer(data, 'binary'));
+			});
 		});
 	});
 });
